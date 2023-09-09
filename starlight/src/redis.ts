c0634c9c0c092121246affb4c40fbfbc3d573078
bweissmann <ben.weissmann15@gmail.com>
2023-09-09 13:33:46 -0700

    Update redis.ts

--- starlight/src/redis.ts
+++ starlight/src/redis.ts
@@ -1,19 +1,36 @@
 import { Tx } from "@/project/context";
 import { vomit } from "@/utils";
+import chalk from "chalk";
 import { createClient } from "redis";
 
 export const redis = await __initializeRedisClient();
 
 async function __initializeRedisClient() {
-  const redisClient = createClient();
-  redisClient.on("error", (err) => console.log("Redis Client Error", err));
-  await redisClient.connect();
-  return redisClient;
+  try {
+    const redisClient = createClient();
+    redisClient.on("error", (err) => {
+      if (err.code === "ECONNREFUSED") {
+        redisClient.quit();
+        throw new Error("Connection refused by Redis server. Events will not be streamed");
+      }
+      console.error("Redis Client Error", err, err.code);
+    });
+    await redisClient.connect();
+    return redisClient;
+  } catch (e: unknown) {
+    if (e instanceof Error) {
+      console.error(chalk.bold(e.message));
+    }
+    return null;
+  }
 }
 
 export type EventType = "LLM_CHAT" | "INIT" | "FN";
 
 export async function emit(tx: Tx, type: EventType, fields: object) {
+  if (redis === null) {
+    return;
+  }
   if (type === "INIT") {
     await redis.xAdd("initializations", "*", {
       id: tx.rx.id,
